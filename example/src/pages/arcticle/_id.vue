e58e89d65b92df794d6e380ee2249d85ec9ae8e4
yezi <18924400146@163.com>
2019-03-25 13:57:31 +0800

    增加路由生成函数

---
+++ example/src/pages/arcticle/_id.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="b">
+    <h1>this is _id</h1>
+
+    <h2>
+      <router-link to="/a">to a</router-link>
+    </h2>
+    <button @click="show">show reqManage</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    show () {
+      for (let item of this.$http.reqManage.show()) {
+        console.log(item)
+      }
+    }
+  }
+}
+</script>
